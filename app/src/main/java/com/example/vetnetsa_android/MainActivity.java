1de88c718829bebd1686fee7f194e38093515e13
reycritt <reycrittenden@gmail.com>
2021-05-21 21:02:51 -0500

    Added a temp method for testing Android commits

--- app/src/main/java/com/example/vetnetsa_android/MainActivity.java
+++ app/src/main/java/com/example/vetnetsa_android/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.vetnetsa_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        method();
+    }
+
+    private void method () {
+        Log.d(TAG, "Temp message!");
     }
 }
